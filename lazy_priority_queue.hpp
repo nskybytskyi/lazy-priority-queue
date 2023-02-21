cb5f5127224c471158f7d37dab022fa2bad6a813
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-02-21 09:51:04 +0200

    chore: added clang-format, formatted source files

--- lazy_priority_queue.hpp
+++ lazy_priority_queue.hpp
@@ -2,46 +2,39 @@
 #include <tuple>
 #include <vector>
 
-template <
-  class T,
-  class Container = std::vector<T>
->
+template <class T, class Container = std::vector<T> >
 class lazy_priority_queue {
-  public:
-    const T& top() const {
-      while (!remove_.empty() && remove_.top() == insert_.top()) {
-        remove_.pop();
-        insert_.pop();
-      }
-      return insert_.top();
-    }
-
-    bool empty() const {
-      return size_ == 0;
-    }
-
-    int size() const {
-      return size_;
-    }
-
-    void push(const T& value) {
-      ++size_;
-      insert_.push(value);
-    }
-
-    void pop() {
-      --size_;
-      std::ignore = top();
+ public:
+  const T& top() const {
+    while (!remove_.empty() && remove_.top() == insert_.top()) {
+      remove_.pop();
       insert_.pop();
     }
+    return insert_.top();
+  }
 
-    void erase(const T& value) {
-      --size_;
-      remove_.push(value);
-    }
+  bool empty() const { return size_ == 0; }
 
-  private:
-    int size_{0};
-    mutable std::priority_queue<T, Container> insert_;
-    mutable std::priority_queue<T, Container> remove_;
+  int size() const { return size_; }
+
+  void push(const T& value) {
+    ++size_;
+    insert_.push(value);
+  }
+
+  void pop() {
+    --size_;
+    std::ignore = top();
+    insert_.pop();
+  }
+
+  void erase(const T& value) {
+    --size_;
+    remove_.push(value);
+  }
+
+ private:
+  int size_{0};
+  mutable std::priority_queue<T, Container> insert_;
+  mutable std::priority_queue<T, Container> remove_;
 };
