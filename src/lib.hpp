36e48564b7b0782d78136afd26a6107d6b089001
Nikita Skybytskyi <n.skybytskyi@gmail.com>
2023-03-04 00:41:57 +0200

    docs: documented and cleaned-up ctors with doxygen

--- src/lib.hpp
+++ src/lib.hpp
@@ -11,18 +11,66 @@ class lazy_priority_queue {
   using value_type = typename Container::value_type;
   using const_reference = typename Container::const_reference;
 
-  lazy_priority_queue() : lazy_priority_queue(Compare()) {}
-  lazy_priority_queue(const Compare& compare)
-      : insert_(compare), remove_(compare) {}
+  /// @brief Default constructor. Value-initializes the comparator and the
+  /// underlying containesr.
+  lazy_priority_queue() : lazy_priority_queue(Compare(), Container()) {}
 
+  /// @brief Copy-constructs the comparison functor with the contents of
+  /// `compare`. Value-initializes the underlying containers.
+  /// @param compare the comparison function object to initialize the
+  /// underlying comparison functor
+  explicit lazy_priority_queue(const Compare& compare)
+      : lazy_priority_queue(compare, Container()) {}
+
+  /// @brief Copy-constructs the underlying insert container from `cont`.
+  /// Value-initializes the underlying remove container. Copy-constructs the
+  /// comparison functor from `compare`. Calls `std::make_heap`.
+  /// @param compare the comparison function object to initialize the
+  /// underlying comparison functor
+  /// @param cont container to be used as source to initialize the underlying
+  /// insert container
+  explicit lazy_priority_queue(const Compare& compare, const Container& cont)
+      : insert_(compare, cont), remove_(compare, Container()) {}
+
+  /// @brief Move-constructs the underlying insert container with
+  /// `std::move(cont)`. Value-initializes the underlying remove container.
+  /// Copy-constructs the comparison functor with `compare`. Calls
+  /// `std::make_heap`.
+  /// @param compare the comparison function object to initialize the
+  /// underlying comparison functor
+  /// @param cont container to be used as source to initialize the underlying
+  /// insert container
+  priority_queue(const Compare& compare, Container&& cont)
+      : insert_(compare, cont), remove_(compare, Container()) {}
+
+  /// @brief Constructs the underlying container from the `{first, last}` range
+  /// and the comparator from `compare`. Calls `std::make_heap`.
+  /// @tparam InputIt must meet the requirements of LegacyInputIterator.
+  /// @param first the beginning of the range of elements to initialize with
+  /// @param last the end of the range of elements to initialize with
+  /// @param compare the comparison function object to initialize the
+  /// underlying comparison functor
   template <class InputIt>
   lazy_priority_queue(InputIt first, InputIt last,
                       const Compare& compare = Compare())
       : insert_(first, last, compare), remove_(compare) {}
+
+  /// @brief Copy-constructs the underlying insert container from `cont`.
+  /// Value-initializes the underlying remove container. Copy-constructs the
+  /// comparison functor from `compare`. Then inserts all elements from the
+  /// `{first, last}` range into the insert container. Finally calls
+  /// `std::make_heap`.
+  /// @tparam InputIt must meet the requirements of LegacyInputIterator.
+  /// @param first the beginning of the range of elements to initialize with
+  /// @param last the end of the range of elements to initialize with
+  /// @param compare the comparison function object to initialize the
+  /// underlying comparison functor
+  /// @param cont container to be used as source to initialize the underlying
+  /// insert container
   template <class InputIt>
   lazy_priority_queue(InputIt first, InputIt last, const Compare& compare,
                       const Container& cont)
-      : insert_(first, last, compare, cont), remove_(compare) {}
+      : insert_(first, last, compare, cont), remove_(compare, Container()) {}
 
   const_reference top() const {
     while (!remove_.empty() && remove_.top() == insert_.top()) {
@@ -62,6 +110,7 @@ class lazy_priority_queue {
   }
 
  private:
+  // TODO: do not duplicate Compare
   mutable std::priority_queue<T, Container, Compare> insert_;
   mutable std::priority_queue<T, Container, Compare> remove_;
 };
